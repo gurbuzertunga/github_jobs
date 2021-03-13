a1bda3fd2d719d57c9b59e6d8652096a3b057814
MkrtichSargsyan <mkrtichsargsyan24@gmail.com>
2021-03-13 18:39:08 +0400

    Add loader component to show loading before fetching data from api

--- src/pages/searchpage.pages.jsx
+++ src/pages/searchpage.pages.jsx
@@ -1,17 +1,19 @@
 import JobList from "../components/jobList";
 import SearchBar from "../components/SearchBar.component";
-import Sidebar from '../components/sidebar'
+import Sidebar from "../components/sidebar";
 
-const SearchPage = ({jobs}) => {
+import Loader from "../components/loader";
+
+const SearchPage = ({ jobs }) => {
   return (
-  <>
-  <SearchBar/>
-  <div className="flex flex-col md:flex-row">
-    <Sidebar page='searchPage'/>
-    <JobList jobs={jobs} />
-  </div>
-  </>
-  )
+    <>
+      <SearchBar />
+      <div className="flex flex-col md:flex-row">
+        <Sidebar page="searchPage" />
+        {jobs ? <JobList jobs={jobs} /> : <Loader />}
+      </div>
+    </>
+  );
 };
 
 export default SearchPage;
